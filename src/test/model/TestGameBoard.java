40f5e45831f987313a538b1bc21f35769799cb9f
Dustin Payne <dp222gr@student.lnu.se>
2021-03-27 14:06:49 +0100

    fails shooting at ship returning the ID

--- src/test/model/TestGameBoard.java
+++ src/test/model/TestGameBoard.java
@@ -1,5 +1,7 @@
 package test.model;
 import main.model.Board;
+import main.model.Orientation;
+import main.model.Ship;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -39,4 +41,36 @@ public class TestGameBoard {
         });
     }
 
+    @Test
+    public void placeShipHorizontalInvalidShouldThrowException(){
+        Board SUT = new Board(6,6);
+        Ship shipStub = new ShipStub(3, 3);
+        assertThrows(IllegalArgumentException.class, ()->{
+            SUT.placeShip(shipStub, 4,5, Orientation.HORIZONTAL);
+        });
+    }
+
+    @Test
+    public void placeShipVerticalInvalidShouldThrowException(){
+        Board SUT = new Board(6,6);
+        Ship shipStub = new ShipStub(3, 3);
+        assertThrows(IllegalArgumentException.class, ()->{
+            SUT.placeShip(shipStub, 5,4, Orientation.VERTICAL);
+        });
+    }
+    @Test
+    public void shootAtShipShouldReturnShipId(){
+        Board SUT = new Board(6,6);
+        Ship shipStub = new ShipStub(3,3);
+        SUT.placeShip(shipStub, 0,1, Orientation.HORIZONTAL);
+        assertEquals(3, SUT.shoot(1,1));
+    }
+
+    static class ShipStub extends Ship{
+
+        public ShipStub(int id, int length) {
+            super(id, length);
+        }
+
+    }
 }
